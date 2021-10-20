cbdeaa7b8d74328a1f1ffd292e0e1f3dac208ee7
Borisevich N. V <borisevich170@yandex.ru>
2021-10-20 10:20:51 +0300

    nodeClassWritten

---
+++ NodeClass.cpp
@@ -0,0 +1,63 @@
+#ifndef MATRIX_MATRIX_H
+#define MATRIX_MATRIX_H
+
+#include <iostream>
+
+template <typename T>
+
+class Node
+{
+    T value;
+    T* next;
+
+public:
+    Node(T val, T* nex);
+    Node(T val);
+    Node(T* nex);
+    Node();
+
+    T val(){return value;}
+    T* nex(){return next;}
+
+
+
+};
+
+template <typename T>
+
+Node<T>::Node(T val, T* nex)
+{
+    value = val;
+    next = next;
+}
+
+template <typename T>
+
+Node<T>::Node(T val)
+{
+    value = val;
+    next = nullptr;
+}
+
+template <typename T>
+
+Node<T>::Node(T* nex)
+{
+    value = 0;
+    next = next;
+}
+
+template <typename T>
+
+Node<T>::Node()
+{
+    value = 0;
+    next = nullptr;
+}
+
+int main()
+{
+    return 0;
+}
+
+#endif
